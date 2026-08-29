8739a9e57128895b68cad52ca93513baffd3b168
hisaabtool <rahul712221@gmail.com>
2026-08-29 22:42:23 +0530

    Add HisaabKit colors

---
+++ app/src/main/java/com/hisaabkit/app/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.hisaabkit.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val HisaabKitPrimary = Color(0xFF2563EB)
+val HisaabKitOnPrimary = Color(0xFFFFFFFF)
+
+val HisaabKitPrimaryContainer = Color(0xFFDBEAFE)
+val HisaabKitOnPrimaryContainer = Color(0xFF172554)
+
+val HisaabKitPrimaryDark = Color(0xFF93C5FD)
+val HisaabKitOnPrimaryDark = Color(0xFF0F172A)
+
+val HisaabKitPrimaryContainerDark = Color(0xFF1E3A8A)
+val HisaabKitOnPrimaryContainerDark = Color(0xFFDBEAFE)
