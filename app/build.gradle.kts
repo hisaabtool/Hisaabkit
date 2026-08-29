29941603a8bcb9c3979767d83426c4456180d33f
hisaabtool <rahul712221@gmail.com>
2026-08-29 22:58:16 +0530

    Add navigation-compose dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.navigation:navigation-compose:2.8.4")
 
     debugImplementation(
         "androidx.compose.ui:ui-tooling"
