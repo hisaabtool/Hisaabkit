19c9654e2c2a3e0b9ef446b7fe2cf6a412af5605
hisaabtool <rahul712221@gmail.com>
2026-08-30 15:15:33 +0530

    Update color definitions for theme

--- app/src/main/java/com/hisaabkit/app/ui/theme/Color.kt
+++ app/src/main/java/com/hisaabkit/app/ui/theme/Color.kt
@@ -2,14 +2,34 @@ package com.hisaabkit.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val HisaabKitPrimary = Color(0xFF2563EB)
-val HisaabKitOnPrimary = Color(0xFFFFFFFF)
+// Brand
+val HisaabKitPrimary = Color(0xFF6750F5)
+val HisaabKitOnPrimary = Color.White
 
-val HisaabKitPrimaryContainer = Color(0xFFDBEAFE)
-val HisaabKitOnPrimaryContainer = Color(0xFF172554)
+val HisaabKitPrimaryContainer = Color(0xFFE8E2FF)
+val HisaabKitOnPrimaryContainer = Color(0xFF21134F)
 
-val HisaabKitPrimaryDark = Color(0xFF93C5FD)
-val HisaabKitOnPrimaryDark = Color(0xFF0F172A)
+// Extra colorful palette
+val HisaabKitPurple = Color(0xFF7C3AED)
+val HisaabKitBlue = Color(0xFF2563EB)
+val HisaabKitCyan = Color(0xFF0891B2)
+val HisaabKitGreen = Color(0xFF059669)
+val HisaabKitOrange = Color(0xFFF97316)
+val HisaabKitPink = Color(0xFFDB2777)
+val HisaabKitRed = Color(0xFFDC2626)
+val HisaabKitYellow = Color(0xFFF59E0B)
 
-val HisaabKitPrimaryContainerDark = Color(0xFF1E3A8A)
-val HisaabKitOnPrimaryContainerDark = Color(0xFFDBEAFE)
+// Light background
+val HisaabKitBackground = Color(0xFFFFFBFF)
+val HisaabKitSurface = Color(0xFFFFFFFF)
+val HisaabKitCard = Color(0xFFF7F3FF)
+
+// Dark theme
+val HisaabKitPrimaryDark = Color(0xFFD0BCFF)
+val HisaabKitOnPrimaryDark = Color(0xFF382E5F)
+
+val HisaabKitPrimaryContainerDark = Color(0xFF4F378B)
+val HisaabKitOnPrimaryContainerDark = Color(0xFFEADDFF)
+
+val HisaabKitBackgroundDark = Color(0xFF141218)
+val HisaabKitSurfaceDark = Color(0xFF1D1B20)
